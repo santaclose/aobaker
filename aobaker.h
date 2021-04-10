7cc09655f667434547324601a2cdfe8518c0c800
santaclose <santidetaramonaf@gmail.com>
2021-04-10 23:31:29 +0200

    Create aobaker.h

---
+++ aobaker.h
@@ -0,0 +1,21 @@
+
+namespace aobaker {
+
+	struct config
+	{
+		int rayCount = 200;
+		bool onlyCastRaysUpwards = true;
+		float rayOriginOffset = 0.001f;
+		float rayDistance = 5.0f;
+		float falloff = 6.0f;
+		float denoiseWeight = 0.3f;
+		int denoisePasses = 1;
+		bool voxelize = true;
+		float voxelSize = 0.01f;
+	};
+
+	void BakeAoToVertices(
+		float* firstVertexPosition, float* firstAoTarget, int vertexCount, size_t stride,
+		unsigned int* indices, int indexCount,
+		const config& conf);
+}
